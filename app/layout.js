f5e848ef5c2ed1d6d8addba6c7d594827f0acb76
Jailson Santos <98654552+jailsonsanttos@users.noreply.github.com>
2026-09-07 23:04:53 -0300

    atualizado app com adsense

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default async function RootLayout({ children }) {
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
             crossOrigin="anonymous"
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
           />
         )}
       </head>
