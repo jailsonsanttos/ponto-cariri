9e8a7fcc9366abbfbd88bd40f273a2126f71ed9a
Jailson Santos <98654552+jailsonsanttos@users.noreply.github.com>
2026-09-13 22:29:58 -0300

    add components atuais

--- components/PropagandaCard.jsx
+++ components/PropagandaCard.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function PropagandaCard({ propaganda }) {
   return (
     <div className="rounded-lg border border-cariri-verde-claro overflow-hidden bg-white flex flex-col">
